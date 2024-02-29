60bd1138859b739a836005a888d9768c53f45ecd
ayan-nair-25 <aan93@scarletmail.rutgers.edu>
2024-02-29 16:32:35 -0500

    Successfully integrated function to build dictionary and check if words are in the dictionary

--- spchk.c
+++ spchk.c
@@ -21,7 +21,10 @@
 // to read the file and annotate position of words:
 
 // modify this to take the double char array as an input so we can avoid needing the struct
-int build_word_dict(char * fname, char ** arr, int n_strings, int initial_word_size) {
+// this isn't working, we need to introduce a triple pointer so that we can modify the actual double array that we pass in 
+// otherwise we need to init the double array inside this function and return this + word size in a struct, which is really inefficient for memory
+int build_word_dict(char * fname, char *** arr, int n_strings, int initial_word_size) {
+
 	int fd = open(fname, O_RDONLY);
 
 	int bytes;
@@ -36,33 +39,30 @@ int build_word_dict(char * fname, char ** arr, int n_strings, int initial_word_s
 		if (idx == n_strings - 1) {
 			// printf("resized array\n");
 			n_strings *= 2;
-			arr = realloc(arr, n_strings * sizeof(char *));
+			(*arr) = realloc((*arr), n_strings * sizeof(char *));
 		}
-		if (arr[idx] == NULL) {
-			arr[idx] = malloc(word_size);
+		if ((*arr)[idx] == NULL) {
+			(*arr)[idx] = malloc(word_size);
 		}
 		if (current_word_idx == word_size - 1) {
 			word_size *= 2;
-			arr[idx] = realloc(arr[idx], word_size);
+			(*arr)[idx] = realloc((*arr)[idx], word_size);
 		}
 		if (buf == '\n') {
-			arr[idx][current_word_idx] = '\0';
+			(*arr)[idx][current_word_idx] = '\0';
 		//	printf("in this case\n");
 			idx++;	
 			current_word_idx = 0;
 			word_size = initial_word_size;
 		}
 		else {
-			arr[idx][current_word_idx++] = buf;
+			(*arr)[idx][current_word_idx++] = buf;
 		}
 
 		//printf("\nloop succeeded\n");
 		//printf("current array index: %d, current word idx: %d\n", idx, current_word_idx);
 	}
 	// debug
-	// for (int i = 0; i < idx; i++) {
-        //	printf("%s\n", arr[i]);
-	//}
 	return idx;
 }
 
@@ -91,6 +91,12 @@ int word_in_dict(char ** dict, char * word, int size) {
 }
 
 // need to include some way to keep track of each word, and put it into the dictionary to see if it is a word
+void reinitialize_word(char * word, int word_size) {
+	free(word);
+	word = NULL;
+	word = malloc(word_size);
+}
+
 void annotate_file(char * fname, char ** dict, int size) {
 	// init the buffer and check the num bytes
 	int fd = open(fname, O_RDONLY);
@@ -112,33 +118,38 @@ void annotate_file(char * fname, char ** dict, int size) {
 		// if we encounter a new line, then we need to move to the next row and reset our column
 
 		// CHANGE THIS 100 TO A MACRO
-		if (word_length == 100) {
+		if (word_length == word_size) {
 			word_size *= 2;
 			current_word = realloc(current_word, word_size);
 		}
 
-		if (!isspace(buf)) {
+		if (!isspace(buf) && !ispunct(buf)) {
 			current_word[word_length++] = buf;
 		}
 
 		if(charcount == 1 && linecount == 1 && !isspace(buf)) {
-			printf("(%d, %d)", linecount, charcount);
+			//printf("(%d, %d)", linecount, charcount);
 		}
 
 		if (buf == '\n') {
-			printf("\n");
+			// printf("\n");
 			linecount++;	
 
 			if (word_length > 0) {
 				current_word[word_length] = '\0';
-				printf("\n Found word %s\n", current_word);
+				// printf("\n Found word |%s|\n", current_word);
+
+				if (word_in_dict(dict, current_word, size)) {
+					// printf("Word in dictionary\n");
+				}
+				else {
+					printf("Word not found: (%d %d) |%s|\n", linecount, charcount, current_word);
+				}
+
+				reinitialize_word(current_word, word_size);
+				word_length = 0;
 			}
 
-			free(current_word);
-			current_word = NULL;
-
-			current_word = malloc(word_size);
-			word_length = 0;
 
 			prev_space = 1;
 			charcount = 1;
@@ -147,27 +158,35 @@ void annotate_file(char * fname, char ** dict, int size) {
 			charcount++;	
 			if (word_length > 0) {
 				current_word[word_length] = '\0';
-				printf("\n Found word %s\n", current_word);
+				// printf("\n Found word |%s|\n", current_word);
+
+				if(word_in_dict(dict, current_word, size)) {
+					// printf("Word in dictionary\n");
+				}
+				else {
+					printf("Word not found: (%d %d) |%s|\n", linecount, charcount, current_word);
+				}
+
+				reinitialize_word(current_word, word_size);
+				current_word = NULL;
+
+				current_word = malloc(word_size);
+				word_length = 0;
 			}
 
-			free(current_word);
-			current_word = NULL;
-
-			current_word = malloc(word_size);
-			word_length = 0;
 
 			prev_space = 1;
 		}
 		else if (prev_space && !isspace(buf)) {
-			printf("\n (%d, %d)", linecount, charcount);
-			printf("%c", buf);
+			//printf("\n (%d, %d)", linecount, charcount);
+			//printf("%c", buf);
 
 			charcount++;
 			prev_space = 0;
 		}
 		// if we get a space, then we encounter a word and hence we print the position
 		else {
-	 		printf("%c", buf);
+	 		//printf("%c", buf);
 			charcount++;
 		}
 	}
@@ -180,7 +199,19 @@ int main(int argc, char ** argv) {
 	}
 
 	char ** dict = malloc(INITIAL_DICT_SIZE * sizeof(char *));
-	int size = build_word_dict(argv[1], dict, INITIAL_DICT_SIZE, INITIAL_WORD_SIZE);
+	int size = build_word_dict(argv[1], &dict, INITIAL_DICT_SIZE, INITIAL_WORD_SIZE);
+
+	/*
+	for (int i = 0; i < size; i++) {
+		if (dict[i] != NULL) {
+			printf("%s\n",dict[i]);
+		}
+		else {
+			printf("failed\n");
+			break;
+		}
+	}
+	*/
 	annotate_file(argv[2], dict, size);
 
 	return EXIT_SUCCESS;
